4d3091dbe78f65547290647f431f0b1e8cf435aa
KatoCode23 <Kalen23Imani@gmail.com>
2017-10-21 18:26:47 -0400

    Ready for second launch....

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
 var app = angular.module('app', []);
 
-app.controller('bodyController', function($scope){
-
-console.log('Hello Sir Xavier!!!');
-});
-
-app.controller('headerController', function($scope){
-
-  $scope.clickbate = [
-      {
-          name: 'Coming Soon...'
-      }
-  ];
-
-$scope.testClick = function(kalen){
-var position1 = $scope.clickbate[0];
-console.log(position1.name);
-}
-});
-
 app.controller('contentController', function($scope){
 
-console.log('Hey, What Up X...');
+  $scope.IsVisible = false;
+  $scope.ShowHide = function () {
+  $scope.IsVisible = $scope.IsVisible ? false : true;
+  }
+  $scope.YouTube = false;
+  $scope.ShowTrailer = function () {
+  $scope.YouTube = $scope.YouTube ? false : true;
+  }
 });
